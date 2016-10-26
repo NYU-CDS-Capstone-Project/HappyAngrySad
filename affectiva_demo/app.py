eeb769f858ec4d53bc31832b6337d6da36c84ec7
Evelyn <evelinka@ngs.ru>
2016-10-26 01:11:02 -0400

    Post to Cloudant

--- affectiva_demo/app.py
+++ affectiva_demo/app.py
@@ -1,6 +1,9 @@
 import os
 from flask import Flask, render_template, send_from_directory
 import json
+import requests
+from requests.auth import HTTPBasicAuth
+
 
 # initialization
 app = Flask(__name__)
@@ -8,28 +11,35 @@ app.config.update(
     DEBUG=True,
 )
 
-# controllers
-
-
-# @app.route('/favicon.ico')
-# def favicon():
-#     return send_from_directory(os.path.join(app.root_path, 'static'), 'ico/favicon.ico')
-
-
-# @app.errorhandler(404)
-# def page_not_found(e):
-#     return render_template('404.html'), 404
-
-@app.route('/getmethod/<jsdata>')
-def get_javascript_data(jsdata):
-	print "trying to get something"
-	return json.loads(jsdata)[0]
+from datetime import timedelta
+from flask import make_response, request, current_app
+from functools import update_wrapper
+import json
 
 @app.route("/")
 def index():
     return render_template('index.html')
 
+@app.route("/postFaces", methods=["POST"])
+def postFaces():
+    print("Will attempt to read 'json'")
 
+    faces = request.json['faces']
+    id = request.json['id']
+    print(faces)
+
+    doc = {
+        '_id': id,
+        'faces': faces
+    }
+
+    auth=HTTPBasicAuth('wonestedidestrowillygeth', 'e635d215e38d37d2847079c1ae4b9584193e24de')
+    headers = {'content-type': 'application/json'}
+    r = requests.post('https://happyangrysad.cloudant.com/emotions', data=json.dumps(doc), auth=auth, headers=headers)
+    print r
+    print r.text
+
+    return "postFaces succeeded!"
 
 # launch
 if __name__ == "__main__":
