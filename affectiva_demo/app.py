eeebef116219859a67a463517e02619956010580
Matthew Dunn <mattyd2@gmail.com>
2016-11-09 13:03:59 -0500

    added comments

--- affectiva_demo/app.py
+++ affectiva_demo/app.py
@@ -16,10 +16,20 @@ from flask import make_response, request, current_app
 from functools import update_wrapper
 import json
 
+
 @app.route('/login', methods=['POST', 'GET'])
 def login():
+    '''
+    Purpose:
+        Handles user email input, checks whether they exist, creates
+        new one if needed, and finally sends them to index.html
+    Parameters:
+        N/A
+    Returns:
+        index.html template
+    '''
     if request.method == 'POST':
-        email = request.form['email']   
+        email = request.form['email']
         if email is None:
             return render_template('login.html')
         if not data.userExists(email):
@@ -30,14 +40,33 @@ def login():
     else:
         return render_template('login.html')
 
+
 @app.route('/logout', methods=['GET'])
 def logout():
+    '''
+    Purpose:
+        redirect method to send user back to login page
+    Parameters:
+        n/a
+    Returns:
+        response with URL redirect and cleared cookie value for username
+    '''
     response = redirect(url_for('login'))
     response.set_cookie('username', '')
     return response
 
+
 @app.route("/")
 def index():
+    '''
+    Purpose:
+        Checks if user is signed-in, redircts to login page if not,
+        and serves serve the index.html if they are signed-in.
+    Parameters:
+        N/A
+    Returns:
+        index.html template
+    '''
     username = request.cookies.get('username')
     if not data.userExists(username):
         return redirect(url_for('login'))
@@ -45,29 +74,72 @@ def index():
         return redirect(url_for('login'))
     return render_template('index.html', username=username)
 
+
 @app.route("/postFaces", methods=["POST"])
 def postFaces():
+    '''
+    Purpose:
+        Receives data from the AJAX call that includes
+        user and view data, which is then posted to the DB.
+    Parameters:
+        N/A
+    Returns:
+        string idicating success
+    '''
+    # get the data from the dictionary sent by AJAX call.
     faces = request.json['faces']
     view_id = request.json['view_id']
 
+    # load the view data from the DB
     view = data.loadView(view_id)
+    # append new views to existing
     view['frames'].append(faces)
+    # save updated view list to DB
     data.saveView(view)
 
     return "success"
 
+
 @app.route("/startView", methods=["POST"])
 def startView():
+    '''
+    Purpose:
+        This is initiated at the beginning of a view session
+        for one video. It receives data from the AJAX call that includes
+        video data, which is then posted to the DB.
+    Parameters:
+        N/A
+    Returns:
+        view_id: string containing the id of the view just created
+    '''
+    # data sent by the AJAX call
     video_id = request.json['video_id']
+    # creates the view record in the DB
     view_id = data.createView(video_id)
     return view_id
 
+
 @app.route("/finishView", methods=["POST"])
 def finishView():
+    '''
+    Purpose:
+        Updates the user record to insert the new view record id
+        created by watching the previous video.
+    Parameters:
+        N/A
+    Returns:
+        succes string to indicate the user record has been updated with the
+        newly created view record id.
+    '''
+    # gets the username from the cookies
     username = request.cookies.get('username')
+    # gets the view_id from the AJAX call
     view_id = request.json['view_id']
+    # gets the user record from DB
     user = data.loadUser(username)
+    # updates user record with view_id
     user['views'].append(view_id)
+    # saves updated user record
     data.saveUser(user)
     return "success"
 
--- affectiva_demo/data.py
+++ affectiva_demo/data.py
@@ -5,80 +5,181 @@ from requests.auth import HTTPBasicAuth
 base_url = "https://happyangrysad.cloudant.com/"
 jsonContentHeaders = {'content-type': 'application/json'}
 
-usersDbAuth = HTTPBasicAuth('dsterentrandellesserywhy', '79ca3b2eba8d9575d50e8ba75c92ec18260bd487')
-emotionsDbAuth = HTTPBasicAuth('wonestedidestrowillygeth', 'e635d215e38d37d2847079c1ae4b9584193e24de')
-viewsDbAuth = HTTPBasicAuth('ateryinummothemedenterne', '6e0320b2deac89677bdd580dc48500ad917afc9e')
+usersDbAuth = HTTPBasicAuth(
+    'dsterentrandellesserywhy', '79ca3b2eba8d9575d50e8ba75c92ec18260bd487')
+emotionsDbAuth = HTTPBasicAuth(
+    'wonestedidestrowillygeth', 'e635d215e38d37d2847079c1ae4b9584193e24de')
+viewsDbAuth = HTTPBasicAuth(
+    'ateryinummothemedenterne', '6e0320b2deac89677bdd580dc48500ad917afc9e')
+
 
 def post(url, data, auth, headers, expected_code):
-	response = requests.post(base_url + url, data=json.dumps(data), auth=auth, headers=headers)
-	if response.status_code != expected_code:
-		raise Exception("Http error:" + response.text)
-	return response
+    '''
+    Purpose:
+        Generic function for creating "POST" URL strings to interact with DB.
+    Parameters:
+        url: this indicates the specific Cloudant DB to access
+        data: defines how you interact with the DB API
+        auth: these are the usernames and keys to the DB
+        headers: determines the format of the response
+        expected_code: this is the expected response from the DB API
+    Returns:
+        The response from the DB API
+    '''
+    response = requests.post(
+        base_url + url, data=json.dumps(data), auth=auth, headers=headers)
+    if response.status_code != expected_code:
+        raise Exception("Http error:" + response.text)
+    return response
+
 
 def get(url, auth):
-	print("GET URL: " + base_url + url)
-	response = requests.get(base_url + url, auth=auth)
-	if response.status_code != 200:
-		raise Exception("Http error:" + response.text)
-	return response
+    '''
+    Purpose:
+        Generic function for creating "GET" URL strings to interact with DB.
+    Parameters:
+        url: this indicates the specific DB to access and the data to get.
+        auth: these are the usernames and keys to the DB
+    Returns:
+        The DB API response containing data
+    '''
+    print("GET URL: " + base_url + url)
+    response = requests.get(base_url + url, auth=auth)
+    if response.status_code != 200:
+        raise Exception("Http error:" + response.text)
+    return response
+
 
 def put(url, data, auth):
-	response = requests.put(base_url + url, data=json.dumps(data), auth=auth)
-	if response.status_code != 201:
-		raise Exception("Http error:" + response.text)	
-	print("Response: " + str(response.status_code))
+    '''
+    Purpose:
+        Generic function for creating "PUT" URL strings to save data to DB.
+    Parameters:
+        url: indicates the specific DB to access and the data create/update
+        data: specific data to be created/updated in DB.
+        auth: these are the usernames and keys to the DB
+    Returns:
+        N/A
+    '''
+    response = requests.put(base_url + url, data=json.dumps(data), auth=auth)
+    if response.status_code != 201:
+        raise Exception("Http error:" + response.text)
+    print("Response: " + str(response.status_code))
+
 
 def userExists(username):
-	query = {
-		"selector": {
-			"login": {
-				"$eq": username
-			}
-		},
-		"fields": [ "login", "views" ]
-	}
-	r = post('users/_find', query, usersDbAuth, jsonContentHeaders, 200)
-	return len(r.json()["docs"]) == 1
+    '''
+    Purpose:
+        Build url to check DB if User exists
+    Parameters:
+        username: string entered by user via login page.
+    Returns:
+        Boolean
+    '''
+    query = {
+        "selector": {
+            "login": {
+                "$eq": username
+            }
+        },
+        "fields": ["login", "views"]
+    }
+    r = post('users/_find', query, usersDbAuth, jsonContentHeaders, 200)
+    return len(r.json()["docs"]) == 1
+
 
 def createUser(username):
-	doc = {
-		'login': username,
-		'views': []
-	}
-	post('users', doc, usersDbAuth, jsonContentHeaders, 201)
+    '''
+    Purpose:
+        Creates the URL string to create user in DB
+    Parameters:
+        username: string input by user on /login page
+    Returns:
+        N/A
+    '''
+    doc = {
+        'login': username,
+        'views': []
+    }
+    post('users', doc, usersDbAuth, jsonContentHeaders, 201)
+
 
 def createView(video_id):
-	doc = {
-		'video': video_id,
-		'frames': []
-	}
-	r = post('views', doc, viewsDbAuth, jsonContentHeaders, 201)
-	id = r.json()['id']
-	if (id is None or len(id) == 0):
-		raise Exception("Failed to create new view")
-	return id
+    '''
+    Purpose:
+        Creates the URL string to create View in DB
+    Parameters:
+        username: string input by user on /login page
+    Returns:
+        N/A
+    '''
+    doc = {
+        'video': video_id,
+        'frames': []
+    }
+    r = post('views', doc, viewsDbAuth, jsonContentHeaders, 201)
+    id = r.json()['id']
+    if (id is None or len(id) == 0):
+        raise Exception("Failed to create new view")
+    return id
+
 
 def loadView(id):
-	r = get("views/" + id, viewsDbAuth)
-	print(r.json())
-	return r.json()
+    '''
+    Purpose:
+        Get the view from DB based upon a given view_id value
+    Parameters:
+        id: id of view
+    Returns:
+        json view record
+    '''
+    r = get("views/" + id, viewsDbAuth)
+    print(r.json())
+    return r.json()
+
 
 def saveView(view):
-	put("views/" + view['_id'], view, viewsDbAuth)
+    '''
+    Purpose:
+        update the view DB with edited view record
+    Parameters:
+        id: id of view
+    Returns:
+        n/a
+    '''
+    put("views/" + view['_id'], view, viewsDbAuth)
+
 
 def loadUser(username):
-	query = {
-		"selector": {
-			"login": {
-				"$eq": username
-			}
-		},
-		"fields": [ "login", "views", "_id", "_rev" ]
-	}
-	r = post('users/_find', query, usersDbAuth, jsonContentHeaders, 200)
-	if (len(r.json()["docs"]) != 1):
-		raise Exception("Error loading user '" + username + "'")
-	return r.json()["docs"][0]
+    '''
+    Purpose:
+        Retrieves from the DB the user based upon the username
+    Parameters:
+        username: the username from the cookie
+    Returns:
+        json view record
+    '''
+    query = {
+        "selector": {
+            "login": {
+                "$eq": username
+            }
+        },
+        "fields": ["login", "views", "_id", "_rev"]
+    }
+    r = post('users/_find', query, usersDbAuth, jsonContentHeaders, 200)
+    if (len(r.json()["docs"]) != 1):
+        raise Exception("Error loading user '" + username + "'")
+    return r.json()["docs"][0]
+
 
 def saveUser(user):
-	put("users/" + user['_id'], user, usersDbAuth)
+    '''
+    Purpose:
+        Updates existing user record in db
+    Parameters:
+        user: id of existing user from DB
+    Returns:
+        n/a
+    '''
+    put("users/" + user['_id'], user, usersDbAuth)
